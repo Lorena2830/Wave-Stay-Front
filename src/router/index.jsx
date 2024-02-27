01e080feb1c7c7fde39eb2c734e668899e2dfcf3
Lorena <lorelore9499@gmail.com>
2024-02-27 16:53:40 +0000

    router modificado

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,11 @@
 import { createBrowserRouter } from "react-router-dom"; 
 import Login from "../pages/Login";
 import Accommodation from "../pages/Accommodation";
+/* import Profile from '../pages/Profile';
+import Signup from '../pages/Signup'
+import Booking from '../pages/Booking'; */
+
+import Root from "../layouts";
 
 const router = createBrowserRouter([
     {
@@ -9,12 +14,24 @@ const router = createBrowserRouter([
     },
     {
         path: '/accommodation',
-        element: <Accommodation/>,
+        element: <Root/>,
         children: [
-           /*  {
+            {
                 path:'/accommodation',
                 element: <Accommodation/>
-            }, */
+            }, 
+            /* {
+                path:'/signup',
+                element: <Signup/>
+            }, 
+            {
+                path:'/booking',
+                element: <Booking/>
+            }, 
+            {
+                path:'/profile',
+                element: <Profile/>
+            },  */
     ]
 }
 ])
